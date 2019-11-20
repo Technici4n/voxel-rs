cdb107b203e0ed9afd98d443fd05ad3aa5af63e3
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2019-11-20 21:48:06 +0100

    Add one level of mipmapping

--- client/src/texture.rs
+++ client/src/texture.rs
@@ -11,15 +11,48 @@ where
     F: gfx::Factory<R>,
     R: gfx::Resources,
 {
+    // Only squared images are allowed
+    // TODO: check for power of two
+    assert_eq!(image.width(), image.height());
+    let image_size = image.width();
+    dbg!(image_size);
+    // Generate mipmaps
+    let mut mipmaps = Vec::new();
+    mipmaps.push(Vec::from(&*image));
+    for level in 1..5 { // 5 mip maps only
+        let current_size = (image_size >> level) as usize;
+        if current_size == 0 {
+            break
+        }
+        let previous_size = (image_size >> (level-1)) as usize;
+        let mut new_layer = Vec::with_capacity(current_size * current_size * 4);
+        let previous_layer = mipmaps.last().unwrap();
+        for row in 0..current_size {
+            for col in 0..current_size {
+                for color in 0..4 {
+                    new_layer.push(
+                        (
+                            (previous_layer[2*row*previous_size*4 + 2*col*4 + color] as u16
+                                + previous_layer[2*row*previous_size*4 + (2*col+1)*4 + color] as u16
+                                + previous_layer[(2*row+1)*previous_size*4 + 2*col*4 + color] as u16
+                                + previous_layer[(2*row+1)*previous_size*4 + (2*col+1)*4 + color] as u16)
+                        / 4) as u8
+                    );
+                }
+            }
+        }
+        mipmaps.push(new_layer);
+    }
+    // Send texture to GPU
     let texture_kind = gfx::texture::Kind::D2(
-        image.width() as u16,
-        image.height() as u16,
+        image_size as u16,
+        image_size as u16,
         gfx::texture::AaMode::Single,
     );
     let (_, texture_view) = factory.create_texture_immutable_u8::<gfx::format::Rgba8>(
         texture_kind,
         gfx::texture::Mipmap::Provided,
-        &[&*image],
+        &[&mipmaps[0], &mipmaps[1], &mipmaps[2], &mipmaps[3], &mipmaps[4]],
     )?;
     Ok(texture_view)
 }
